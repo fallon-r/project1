f3c27c09e6c14c1ea713fa2d4dd5ce18c4709a70
rf-spuds <59809797+rf-spuds@users.noreply.github.com>
2020-03-16 20:00:39 -0400

    Delete splash.js

--- splash.js
+++
@@ -1,9 +0,0 @@
-var queryURL = "https://api.the-odds-api.com/v3/sports/?apiKey=1194b7ea53a49fad946f0bc020a4d57c"
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-});
