d4d0cb4e4c290bd559b540ad4603f4d36433659d
rf-spuds <rfspuds@gmail.com>
2020-03-21 11:55:39 -0400

    added &all to pull up all active sports

--- odds.js
+++ odds.js
@@ -3,13 +3,14 @@ var region = navigator.language.slice(3, 5).toLowerCase();
 var odds = "h2h";
 
 
-var queryURL = "https://api.the-odds-api.com/v3/odds/?apiKey=" + key + "&sport=upcoming" + "&region=" + region + "&mkt=" + odds;
+var queryURL = "https://api.the-odds-api.com/v3/sports/?apiKey=" + key + "&all=1";
+// + "&sport=upcoming" + "&mkt=" + odds;
 
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
-    console.log(response)
-    console.log
+    console.log(response);
+    console.log(queryURL)
 });
