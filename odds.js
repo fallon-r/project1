386768f86c82fdf57ea26c0873fafb0f8019dca6
rf-spuds <rfspuds@gmail.com>
2020-03-22 23:03:05 -0400

    Odds branch status push

--- odds.js
+++ odds.js
@@ -1,15 +1,51 @@
 var key = "1194b7ea53a49fad946f0bc020a4d57c";
+
+// defaults to US, but uses browser language to get bookmakers
 var region = navigator.language.slice(3, 5).toLowerCase();
-var odds = "h2h";
+//Blank array for sport status location
+var sportActivity;
+
+//special URL that returns whether sport is in or out of season.
+var sportStatus = "https://api.the-odds-api.com/v3/sports/?apiKey=" + key + "&all=1"
+
+function getStatus() {
+    $.ajax({
+        url: sportStatus,
+        method: "GET"
+    }).then(function(response) {
+        sportActivity = response;
+    });
+
+};
 
 
-var queryURL = "https://api.the-odds-api.com/v3/odds/?apiKey=" + key + "&sport=upcoming" + "&region=" + region + "&mkt=" + odds;
+// The array becomes filled with the status of all sports. they are in alphabetical order so indices are static.
+getStatus();
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-    console.log
+function mlb() {};
+
+$("#baseball").click(function(event) {
+    event.preventDefault();
+    mlb();
+});
+
+$("#basketball").click(function(event) {
+    event.preventDefault();
+    mlb();
+});
+
+$("#football").click(function(event) {
+    event.preventDefault();
+    mlb();
+});
+
+$("#hockey").click(function(event) {
+    event.preventDefault();
+    mlb();
+});
+
+$("#ufc").click(function(event) {
+    event.preventDefault();
+    mlb();
 });
